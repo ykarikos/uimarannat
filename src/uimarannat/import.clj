d43cc5490b00257ca73d6c4e19fe82295476b433
Yrjö Kari-Koskinen <ykk@peruna.fi>
2014-04-03 23:48:04 +0300

    store data in mongodb

--- src/uimarannat/import.clj
+++ src/uimarannat/import.clj
@@ -1,4 +1,6 @@
-(ns uimarannat.import)
+(ns uimarannat.import
+	(:require [monger.collection :as mc]
+			  [monger.core :as mg]))
 
 (use 'clojure.java.io)
 (use '[clojure.string :only (split)])
@@ -8,13 +10,20 @@
 	(let [[date name coordinates description] (split line #"\t")
 		  [lat lon] (map read-string (split coordinates #","))]
 		(if (and (number? lat) (number? lon))
-			(str lat ":" lon)
-			"")))
+			{:date date
+			 :name name
+			 :lat lat
+			 :lon lon
+			 :description description})))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (mg/connect!)
+  (mg/set-db! (mg/get-db "uimarannat"))
   (with-open [rdr (reader "uimapaikkakysely.tsv")]
   	(doseq [line (line-seq rdr)]
-  		(println (process-line line)))))
+  		(let [data (process-line line)]
+  			(if data
+	  			(mc/insert "rannat" (process-line line)))))))
 
