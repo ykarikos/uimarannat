122f5ba0c1a3a554b6e54a04187a663d6e05344f
Yrjö Kari-Koskinen <ykk@peruna.fi>
2014-04-05 22:38:03 +0300

    use GeoJSON format

--- src/uimarannat/import.clj
+++ src/uimarannat/import.clj
@@ -5,6 +5,13 @@
 (use 'clojure.java.io)
 (use '[clojure.string :only (split)])
 
+; query like this:
+; db.runCommand( { geoNear: "rannat", 
+;	near: {type: "Point", 
+;		coordinates: [21.5228, 61.5643] }, 
+;		spherical: true, 
+;		maxDistance: 10 }).results.length
+
 (defn process-line
 	[line]
 	(let [[date name coordinates description] (split line #"\t")
@@ -12,12 +19,11 @@
 		(if (and (number? lat) (number? lon))
 			{:date date
 			 :name name
-			 :lat lat
-			 :lon lon
+			 :location {:type "Point" :coordinates [lon, lat]}
 			 :description description})))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Save data in a GeoJSON database"
   [& args]
   (mg/connect!)
   (mg/set-db! (mg/get-db "uimarannat"))
@@ -25,5 +31,5 @@
   	(doseq [line (line-seq rdr)]
   		(let [data (process-line line)]
   			(if data
-	  			(mc/insert "rannat" (process-line line)))))))
+	  			(mc/insert "rannat" data))))))
 
